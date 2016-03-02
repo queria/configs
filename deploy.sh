fe0f86b1b51df3e6a9f45c786b4e98346b48f717
Pavel Sedlák <psedlak@redhat.com>
2016-03-02 11:23:18 +0100

    add support for sudo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-BUPD=$(mktemp -d --tmpdir "config-backup-XXXXXX")
-BUP_USED=false
-CHANGED=false
+export BUPD=$(mktemp -d --tmpdir "config-backup-XXXXXX")
+export BUP_USED="${BUPD}/__bup_used"
+export CHANGED="${BUPD}/__changed"
 
-EX_CLONE=11
-EX_MKDIR=12
+export EX_CLONE=11
+export EX_MKDIR=12
 
-CLR_ERROR="\033[1;31m"
-CLR_ALREADY="\033[0;32m"
-CLR_NONE="\033[00m"
+export CLR_ERROR="\033[1;31m"
+export CLR_ALREADY="\033[0;32m"
+export CLR_NONE="\033[00m"
+
+export MYBIN="$(dirname "$(readlink -f "$0")")/_bin"
+export PATH="$MYBIN:${PATH}"
 
 safetycheck() {
     if [[ "$1" != "--force" && "$HOME" != "$(echo ~)" ]]; then
@@ -17,98 +20,39 @@ safetycheck() {
         echo "You can use --force to override this check."
         exit 1
     fi
-}
-fin_message() {
-    if [[ ! -z "$1" ]]; then
-        echo -e "${CLR_ERROR}** FAILED: $1 **${CLR_NONE}"
-    elif ! $CHANGED; then
-        echo ""
-        echo -e "${CLR_ALREADY}Seems all configs are already deployed here.${CLR_NONE}"
-        echo ""
-    fi
-    if $BUP_USED; then
-        echo ""
-        echo "All original files were moved to"
-        echo "$BUPD"
-        echo "You can inspect it, opt. merge with deployed configs,"
-        echo "and delete the directory"
-    else
-        rmdir "$BUPD"
-    fi
-    if [[ ! -z "$2" ]]; then
-        exit $2
+    if [[ -z "$BUPD" ]]; then
+        echo "Issue with getting temporary backup dir!"
+        exit 1
     fi
 }
 safemkdir() {
-    mkdir -vp "$1" || fin_message "mkdir $1" $EX_MKDIR
-}
-bup() {
-    BUP_USED=true
-    mv -v "$1" "${BUPD}/${1//\//_}"
-}
-bup_if_not_repo() {
-    if [[ -d "$1" ]]; then
-        pushd "$1" > /dev/null
-        git status &> /dev/null
-        local rc="$?"
-        popd > /dev/null
-        if [[ "$rc" = "128" ]]; then
-            bup "$1"
-        fi
-    fi
-}
-deplink() {
-    local src="$(readlink -f $1)"
-    local tgt="$2"
-    if [[ -f "$tgt" ]]; then
-        if [[ -L "$tgt" ]]; then
-            src_tgt="$(readlink -f "$tgt")"
-            if [[ "$src" = "$src_tgt" ]]; then
-                echo -e "${CLR_ALREADY}‘$tgt’ -> ‘$src’ [already]${CLR_NONE}";
-                # link already matches
-                return
-            fi
-        fi
-        if diff "$src" "$tgt" &> /dev/null; then
-            echo "Replacing $tgt with symlink [same content]"
-            rm "$tgt"
-        else
-            bup "$tgt"
-        fi
-    fi
-    CHANGED=true
-    if [[ ! -e "$tgt" ]]; then
-        ln -vsnf "$src" "$tgt"
-    else
-        echo "Unable to deploy ${src} as ${tgt} (not a file...), please inspect and fix."
+    if ! mkdir -vp "$1"; then
+        fin_message "mkdir $1"
+        exit $EX_MKDIR
     fi
 }
 deprepo() {
-    local repo=$1
-    local tgt=$2
-    local deep=false
-    if [[ "$3" = "submodules" ]]; then
-        deep=true
-    fi
-
-    bup_if_not_repo "$tgt"
-
-    if [[ ! -d "$tgt" ]]; then
-        CHANGED=true
-        echo "‘$tgt’ -> ‘$repo’ [cloning]";
-        git clone --depth=1 "$repo" "$tgt" || fin_message "Cloning $repo into $tgt" $EX_CLONE
-        if $deep; then
-            pushd "$tgt" > /dev/null
-            (git submodule init && git submodule update --remote) || fin_message "Submodule init/update in $tgt"
-            popd > /dev/null
-        fi
-    else
-        echo -e "${CLR_ALREADY}‘$tgt’ -> ‘$repo’ [already]${CLR_NONE}";
+    if ! "$MYBIN/deprepo" "$@"; then
+        fin_message "Cloning $repo into $tgt"
+        exit $EX_CLONE
     fi
 }
+deplink() {
+    "$MYBIN/deplink" "$@"
+}
+sudo_deprepo() {
+    if ! sudo -E "$MYBIN/deprepo" "$@"; then
+        fin_message "Cloning $repo into $tgt"
+        exit $EX_CLONE
+    fi
+}
+sudo_deplink() {
+    sudo -E "$MYBIN/deplink" "$@"
+}
+
 deploy_repos() {
     if [[ ! -d "$HOME/all/src" ]]; then
-        CHANGED=true
+        touch "$CHANGED"
         mkdir -vp "$HOME/all/src"
     fi
     deprepo git@github.com:queria/scripts.git "$HOME/all/src/scripts"
@@ -149,6 +93,8 @@ deploy_configs() {
     deplink mpv "$HOME/.mpv/config"
 
     deplink "$HOME/.vim/qs_vimrc" "$HOME/.vimrc"
+
+    sudo_deplink vok "/usr/share/X11/xkb/symbols/vok"
 }
 
 
@@ -159,5 +105,3 @@ deploy_repos
 deploy_configs
 
 fin_message
-
-
