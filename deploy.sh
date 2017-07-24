06ec8ef1b4b6f0e67d5254d3c724764083e2afe0
Pavel Sedlák <psedlak@redhat.com>
2017-07-24 16:17:41 +0200

    bin_check for "scrot" command

--- deploy.sh
+++ deploy.sh
@@ -172,6 +172,7 @@ check_app_presence() {
     bin_check virtualenv
     bin_check flake8 "python{,3}-flake8"
     bin_check openstack "python-openstackclient"
+    bin_check scrot
     cmd_check 'python -c "import jedi"' "python{,3}-jedi"
     cmd_check '[[ "$SHELL" =~ .*zsh ]]' "zsh as user shell"
 }
